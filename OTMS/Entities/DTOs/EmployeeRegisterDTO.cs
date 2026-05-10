088c78525be1aa58fa442a42d79a8aae8a2cd36c
IAmHaiel <mikhjnelo@gmail.com>
2026-05-10 13:48:40 +0800

    Added Formatting and Data Validation for Contact Number in Registering Accounts

--- OTMS/Entities/DTOs/EmployeeRegisterDTO.cs
+++ OTMS/Entities/DTOs/EmployeeRegisterDTO.cs
@@ -8,9 +8,6 @@ namespace OTMS.Entities.DTOs
 
         public string EmployeeName { get; set; }
 
-        [RegularExpression(
-            @"^09\d{9}$",
-            ErrorMessage = "Contact Number must be exactly 11 digits and start with 09.")]
         public string ContactNumber { get; set; }
 
         public string Role { get; set; }
