4066602c5611955716b561bee8b4789d88ac53a7
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 11:24:37 +0800

    Added the Implementation in the Controller

--- OTMS/Controllers/leaverequestController.cs
+++ OTMS/Controllers/leaverequestController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OTMS.Entities.DTOs.LeaveRequest;
+using OTMS.Service.Interfaces;
 
 namespace OTMS.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class leaverequestController : ControllerBase
+    public class leaverequestController(ILeaveRequest leaveRequest) : ControllerBase
     {
+
+        /// <summary>
+        /// The system shall allow Operational Team members to submit a formal time-off request by specifying the start date, end date, and reason.
+        /// </summary>
+        [Authorize(Roles = "SystemAdmin,OperationAdmin,Coordinator,Encoder")]
+        [HttpPost("create-leave-request")]
+        public async Task<IActionResult> CreateLeaveRequest(CreateLeaveRequestDTO request)
+        {
+            try
+            {
+                var result = await leaveRequest.CreateLeaveRequestAsync(request);
+                return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+
     }
 }
