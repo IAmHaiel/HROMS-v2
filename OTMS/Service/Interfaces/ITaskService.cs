7e756487c1c866e7ed0cb388566fee91d766856b
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 16:33:26 +0800

    Added interface for task

---
+++ OTMS/Service/Interfaces/ITaskService.cs
@@ -0,0 +1,10 @@
+﻿using OTMS.Entities.DTOs.Task;
+using OTMS.Entities.DTOs.Task.Responses;
+
+namespace OTMS.Service.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<TaskResponseDTO> CreateTaskAsync(CreateTaskDTO request);
+    }
+}
