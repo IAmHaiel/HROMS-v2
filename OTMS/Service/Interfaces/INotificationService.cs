8f997631c1042332dece4a9b01f5fc4cc9ddcebb
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 17:07:24 +0800

    Implemented a Interface for Notification Service

---
+++ OTMS/Service/Interfaces/INotificationService.cs
@@ -0,0 +1,8 @@
+﻿namespace OTMS.Service.Interfaces
+{
+    public interface INotificationService
+    {
+        Task CreateTaskAssignedNotificationAsync(Task task);
+        Task CreateDeadlineNotificationAsync(Task task);
+    }
+}
