05510f6ad39425acf91ec325a0d9920aec0d045d
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 11:12:33 +0800

    Added Interface, Service, and Controller for LeaveRequest

---
+++ OTMS/Controllers/leaverequestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OTMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class leaverequestController : ControllerBase
+    {
+    }
+}
---
+++ OTMS/Service/Services/LeaveRequestService.cs
@@ -0,0 +1,12 @@
+﻿using OTMS.Data;
+using OTMS.Service.Interfaces;
+
+namespace OTMS.Service.Services
+{
+    public class LeaveRequestService (
+        OTMSDbContext context,
+        IHttpContextAccessor httpContextAccessor
+        ) : ILeaveRequest
+    {
+    }
+}
