d14b7e5578e9f2a5240d79232ffc0d615a05996d
IAmHaiel <mikhjnelo@gmail.com>
2026-06-06 20:00:23 +0800

    Added DTO for ResetPassword

---
+++ OTMS/Entities/DTOs/ResetPassword/ResetPasswordDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace OTMS.Entities.DTOs.ResetPassword
+{
+    public class ResetPasswordDTO
+    {
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
