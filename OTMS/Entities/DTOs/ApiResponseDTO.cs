9a4bf5c6c27a8c93af2c5933ec75f14c6d24adc7
IAmHaiel <mikhjnelo@gmail.com>
2026-06-06 12:00:14 +0800

    Added Generic ApiResponseDTO

---
+++ OTMS/Entities/DTOs/ApiResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OTMS.Entities.DTOs
+{
+    public class ApiResponseDTO<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+}
