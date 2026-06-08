8170be68eca1d786f47d2dace3df6aa9002a3cc8
IAmHaiel <mikhjnelo@gmail.com>
2026-06-08 12:56:27 +0800

    Enforced Strict Email and Contact Number Validation in Updating Accounts

--- OTMS/Entities/DTOs/Profile/Responses/UpdateInformationResponseDTO.cs
+++ OTMS/Entities/DTOs/Profile/Responses/UpdateInformationResponseDTO.cs
@@ -8,6 +8,7 @@
         public string LastName { get; set; } = string.Empty;
         public string? Suffix { get; set; } = null;
         public string ContactNumber { get; set; } = string.Empty;
+        public string Email {  get; set; } = string.Empty;
 
         public DateTime UpdatedAt { get; set; }
 
