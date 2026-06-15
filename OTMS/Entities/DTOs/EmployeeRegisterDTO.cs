0dfe63cc2e8664825b111f5c9039b756923f8999
IAmHaiel <mikhjnelo@gmail.com>
2026-06-16 02:20:00 +0800

    blind-provisioning-sprint-3.2

--- OTMS/Entities/DTOs/EmployeeRegisterDTO.cs
+++ OTMS/Entities/DTOs/EmployeeRegisterDTO.cs
@@ -7,6 +7,12 @@ namespace OTMS.Entities.DTOs
         public string EmployeeNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        [Required]
+        public Guid DepartmentId { get; set; }
+        [Required]
+        public Guid JobPositionId { get; set; }
+        [Required]
+        public string EmploymentStatus { get; set; } = string.Empty;
         public List<Microsoft.AspNetCore.Http.IFormFile>? Attachments { get; set; }
     }
 }
