fd81c9a457126cba8bd955d18062c065121bba4d
IAmHaiel <mikhjnelo@gmail.com>
2026-06-19 01:49:57 +0800

    Refactored Password Generation

--- OTMS/Service/Helper/PasswordGenerator.cs
+++ OTMS/Service/Helper/PasswordGenerator.cs
@@ -6,59 +6,46 @@ namespace OTMS.Service.Helper
 {
     public static class PasswordGenerator
     {
-        private static readonly string[] WordList =
-        {
-            "river",
-            "mountain",
-            "orange",
-            "falcon",
-            "galaxy",
-            "thunder",
-            "banana",
-            "shadow",
-            "tiger",
-            "forest",
-            "rocket",
-            "diamond",
-            "ocean",
-            "planet",
-            "sunrise",
-            "whisper",
-            "coconut",
-            "volcano",
-            "phoenix",
-            "silver"
-        };
+        private const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+        private const string Digits = "0123456789";
+        private const string Symbols = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+        private const string AllChars = Uppercase + Lowercase + Digits + Symbols;
 
         public static string Generate(int minimumLength = PasswordLength.MinimumLength)
         {
             if (minimumLength < PasswordLength.MinimumLength)
-            {
-                throw new ArgumentException(
-                    "Passphrase must be at least 15 characters long."
-                );
-            }
+                throw new ArgumentException($"Password must be at least {PasswordLength.MinimumLength} characters long.");
 
-            var words = new List<string>();
-            int currentLength = 0;
+            var password = new char[minimumLength];
+            var randomBytes = new byte[minimumLength];
 
-            while (currentLength < minimumLength)
-            {
-                string word = GetRandomWord();
+            RandomNumberGenerator.Fill(randomBytes);
 
-                words.Add(word);
+            password[0] = PickRandomChar(Uppercase, randomBytes[0]);
+            password[1] = PickRandomChar(Lowercase, randomBytes[1]);
+            password[2] = PickRandomChar(Digits, randomBytes[2]);
+            password[3] = PickRandomChar(Symbols, randomBytes[3]);
 
-                currentLength = string.Join("-", words).Length;
-            }
+            for (int i = 4; i < minimumLength; i++)
+                password[i] = PickRandomChar(AllChars, randomBytes[i]);
 
-            return string.Join("-", words);
+            var shuffled = Shuffle(password, randomBytes);
+            return new string(shuffled);
         }
 
-        private static string GetRandomWord()
-        {
-            int index = RandomNumberGenerator.GetInt32(WordList.Length);
+        private static char PickRandomChar(string charset, byte seed) =>
+            charset[seed % charset.Length];
 
-            return WordList[index];
+        private static char[] Shuffle(char[] chars, byte[] randomBytes)
+        {
+            for (int i = chars.Length - 1; i > 0; i--)
+            {
+                int j = randomBytes[i] % (i + 1);
+                (chars[i], chars[j]) = (chars[j], chars[i]);
+            }
+            return chars;
         }
     }
 }
